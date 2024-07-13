9887d5080ba0c7fd52fcfce8c4a7c49e9d78103f
Aman Pannu <amanpannu2107@gmail.com>
2024-07-13 23:12:01 +0530

    Add files via upload

---
+++ Assignment10.cpp
@@ -0,0 +1,24 @@
+//Question 1
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a,b;
+//     cout<<"enter the value : ";
+//     cin>>a>>b;
+//     int *ptra=&a;
+//     int *ptrb=&b;  
+//     cout<<(*ptra)*(*ptrb);
+// }
+
+//Question 2
+//  1 ->  p is a pointer and q is a integer
+
+//Question 3
+//  3 -> ( 11 10 11 )
+
+//Question 4
+//  a get the value of b 
+
+//Question 5
+//  code is right but gives runtime error ptr get garbage address
+
